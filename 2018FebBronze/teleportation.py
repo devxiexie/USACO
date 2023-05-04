c7c82116d69b6f2d29648f252d118208bc63a0e7
funnoodle11 <81544214+funnoodle11@users.noreply.github.com>
2023-05-03 20:41:36 -0700

    Create teleportation.py

---
+++ 2018FebBronze/teleportation.py
@@ -0,0 +1,15 @@
+file = open("teleport.in", "r")
+output = open("teleport.out", "w")
+line=file.readline()
+file.close()
+
+line=list(line.split(" "))
+
+a,b,x,y=int(line[0]),int(line[1]),int(line[2]),int(line[3])
+
+answer = abs(a-b)
+answer = min(answer, abs(a-x) + abs(b-y))
+answer = min(answer, abs(a-y) + abs(b-x))
+output.write(str(answer))
+
+output.close()
